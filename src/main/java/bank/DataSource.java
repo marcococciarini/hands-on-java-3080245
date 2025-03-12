2510dc477f77327ba3dde9d85802dee7de765985
marcococciarini <156895913+marcococciarini@users.noreply.github.com>
2025-03-12 10:41:02 +0000

    Completed dB access in DataSourceclass to retrieve customer given a username

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -19,7 +21,36 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username){
+    String sql = "select * from Customers where username = ?";
+    Customer customer = null;
+
+    try(Connection connection = connect();
+        PreparedStatement stmt = connection.prepareStatement(sql)){
+
+          stmt.setString(1, username);
+          try (ResultSet resultSet = stmt.executeQuery()){
+            customer = new Customer(
+                                    resultSet.getInt("id"), 
+                                    resultSet.getString("name"),
+                                    resultSet.getString("username"),
+                                    resultSet.getString("password"),
+                                    resultSet.getInt("account_id") );
+
+              
+          }
+
+    } catch (SQLException exc) {
+      exc.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args){
-    connect();
+    //connect();
+
+    Customer myCustomer = getCustomer("twest8o@friendfeed.com");
+    System.out.println("RETRIEVED CUSTOMER NAME = " + myCustomer.getName());
   }
 }
